ab78245be842aa240be4aade8ec27c6c2fffae9b
wangyongyi <2664139264@qq.com>
2024-06-14 15:32:22 +0000

    添加了log功能

--- __main__.py
+++ __main__.py
@@ -1,11 +1,20 @@
 import gymnasium as gym
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env.base_vec_env import VecEnv
+from stable_baselines3.common.vec_env import VecMonitor
+from stable_baselines3.common.monitor import Monitor
 
-from experiment.dqn import run_dqn
+from experiment.dqn import run_dqn, log_root
 
 
 if __name__ == "__main__":
-    env = gym.make("LunarLander-v2", render_mode="rgb_array")
-
-    result = run_dqn(env)
-
+    env = make_vec_env("LunarLander-v2", n_envs = 5)
+    env_monitor_args = dict(
+        filename = log_root,
+        info_keywords = (),
+    )
+    env = (VecMonitor if isinstance(env, VecEnv) else Monitor)(env, **env_monitor_args)
+    eval_env = make_vec_env("LunarLander-v2", n_envs = 5)
+    eval_env = (VecMonitor if isinstance(env, VecEnv) else Monitor)(env, **env_monitor_args)
+    result = run_dqn(env, eval_env)
     print(result)
--- experiment/dqn.py
+++ experiment/dqn.py
@@ -1,22 +1,53 @@
+import os
+from copy import deepcopy
 from typing import Callable, Dict, Any
 from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback, CallbackList
+
 from utils import AlgoT, EnvT, PolicyInstanceT, EnvInstanceT
 
-
-dqn_algo_args = dict(verbose=1)
-dqn_learn_args = dict(total_timesteps=int(1e3), progress_bar=True)
-dqn_eval_args = dict(n_eval_episodes=10)
+log_root = "./log/dqn"
+dqn_algo_args = dict(
+    verbose = 1,
+    tensorboard_log = log_root
+)
+dqn_learn_args = dict(
+    total_timesteps = int(1e5),
+    progress_bar = True
+)
+dqn_eval_args = dict(
+    n_eval_episodes = 10
+)
+eval_callback_args = dict(
+    best_model_save_path = os.path.join(log_root, "ckpt"),
+    log_path = os.path.join(log_root, "eval_log"),
+    eval_freq = 500,
+    deterministic = True,
+    render = False
+)
 
 default_policy_constructor = lambda _, __: "MlpPolicy"
 
-def run_dqn(env: EnvInstanceT,
+def run_dqn(env: EnvInstanceT, eval_env: EnvInstanceT,
         policy_constructor: Callable[[AlgoT, EnvT], PolicyInstanceT] = default_policy_constructor,
         algo_args: Dict[str, Any] = dqn_algo_args,
         learn_args: Dict[str, Any] = dqn_learn_args,
         eval_args: Dict[str, Any] = dqn_eval_args):
+
     policy = policy_constructor(DQN, env.__class__)
-    model = DQN(policy, env, **algo_args)
-    model.learn(**learn_args)
+    model = DQN(
+        policy, env, 
+        **algo_args,
+    )
+    model.learn(
+        **learn_args,
+        callback = CallbackList([
+            EvalCallback(
+                eval_env,
+                **eval_callback_args
+            )
+        ])
+    )
     mean_reward, std_reward = evaluate_policy(model, model.get_env(), **eval_args)
     return mean_reward, std_reward
