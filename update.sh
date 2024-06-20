6da329d1df243139d23e729726974c6c5982accb
wangyongyi <2664139264@qq.com>
2024-06-20 05:45:53 +0000

    updated

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 COMMIT_INFO=""
-read -t 100 -p "Input Commit Info: " COMMIT_INFO
+read -t 1024 -p "Input Commit Info: " COMMIT_INFO
 
-for DIR in ./stable-baselines3 ../stable-baselines-contrib ../rl-baselines3-zoo ..
+for DIR in ./stable-baselines3 ../stable-baselines3-contrib ../rl-baselines3-zoo ..
 do
     cd $DIR
     echo "Commit $DIR"
+    git add .
     git commit -m $COMMIT_INFO
     git push
 done
