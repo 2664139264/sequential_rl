c6fea7cb690a757290e5ebd39d34599b2e97a7c7
wangyongyi <2664139264@qq.com>
2024-06-20 16:05:05 +0000

    updated

--- update.sh
+++ update.sh
@@ -6,8 +6,9 @@ read -t 1024 -p "Input Commit Info: " COMMIT_INFO
 for DIR in ./stable-baselines3 ../stable-baselines3-contrib ../rl-baselines3-zoo ..
 do
     cd $DIR
-    echo "Commit $DIR"
+    echo "******* Commit Module $DIR *******"
     git add .
     git commit -m $COMMIT_INFO
     git push
+    echo "******* Commit Completed. *******"
 done
